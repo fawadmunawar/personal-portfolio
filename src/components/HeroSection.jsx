4402f108e2c3fb31541bb8127e8ada8c052e7dd0
Fawad Munawar <fawadmunawer@gmail.com>
2024-08-01 13:34:28 +0500

    hero section added

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -9,7 +9,7 @@ const HeroSection = () => {
             <div className='absolute inset-0 z-10 bg-gradient-to-b from-transparent from-60% to-black lg:from-30%'></div>
             <div className='z-20 mx-4 max-w-3xl pb-8 absolute bottom-1'>
                 <h1 className='text-5xl font-semibold uppercase tracking-wide md:text-7xl'>Fawad Munawar</h1>
-                <p className='pt-2 font-semibold'>Front-end developer specializing in sleek and responsive design.</p>
+                <p className='pt-2 font-semibold text-gray-300'>Front-end developer specializing in sleek and responsive design.</p>
             </div>
         </div>
     </>
